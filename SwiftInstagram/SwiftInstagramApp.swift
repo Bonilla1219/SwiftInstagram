c93db51013dfc559440925eac30561e9adc3feb3
Javierb1219 <bonillaculture1219@gmail.com>
2023-06-14 18:03:24 -0700

    Initial Commit

---
+++ SwiftInstagram/SwiftInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftInstagramApp.swift
+//  SwiftInstagram
+//
+//  Created by Javier Bonilla on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
