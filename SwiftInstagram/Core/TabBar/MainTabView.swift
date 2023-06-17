273de81884231be23bee50e1c1aa43a6f2b16333
Javierb1219 <bonillaculture1219@gmail.com>
2023-06-16 17:56:44 -0700

    Added profile design and layout.

---
+++ SwiftInstagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,47 @@
+//
+//  MainTabView.swift
+//  SwiftInstagram
+//
+//  Created by Javier Bonilla on 6/14/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        //TabView sets the tabs at the bottom of the screen
+        TabView{
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
